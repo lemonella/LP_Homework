056b82ae4f436018d2cc13730c5de24aa6254df9
lemonella <widder3@yandex.ru>
2017-03-24 15:42:55 +0300

    Planets, wordscount and some stuff

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from planet import *
+from wordcount import *
 
 def main():
     updater = Updater("315853602:AAHhawei6okVUMQkcLgR13VC4Zi6BpWG944")
@@ -21,7 +22,17 @@ def show_error (bot, update, error):
     print(error)
 
 def talk_to_me(bot, update):
-    sign = get_sign_by_planet(update.message.text)
-    bot.sendMessage(update.message.chat_id, sign)
+    answer = ''
+
+    if message.startswith ('/wordcount'):
+        answer = count_words_in_message(update.message.text)
+        print(1, answer)
+    else:
+        answer = get_sign_by_planet(update.message.text)
+        print(2, answer)
+
+    print(3, answer)
+    
+    bot.sendMessage(update.message.chat_id, answer)
 
 main()
---
+++ bot/wordcount.py
@@ -0,0 +1,26 @@
+'''
+Добавить команду /wordcount котрая считает сова в присланной фразе. 
+Например на запрос /wordcount "Привет как дела" бот должен посчитать количество слов в кавычках и ответить: 3 слова.
+'''
+
+# Function returns number of words in the message
+import re
+
+def count_words_in_message (message):
+	result = -1
+
+	if not message.startswith ('/wordcount'):
+		return result
+
+	bare_phrase = re.findall('\"(.*)\"', message)
+	if bare_phrase:
+		result = len(bare_phrase[0].split())
+	else:
+		result = "Не удалось посчитать слова. Фраза для подсчета слов должна быть в кавычках и не должна быть пустой"
+
+	return result
+
+
+if __name__ == '__main__':
+	# print(count_words_in_message(input('Введи фразу: ')))
+	print(count_words_in_message('/wordcount sfhvlskjfhgsl'))
